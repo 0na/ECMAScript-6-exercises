75f595713d61b83e41eac6d5e8ee70ed6ceab89b
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-02-25 11:30:40 +0100

    task3 finished

--- Zadanie3.js
+++ Zadanie3.js
@@ -1,7 +1,4 @@
-//Napisz funkcję average, która obliczy średnią arytmetyczną wszystkich argumentów, które zostaną do niej przekazane. Załóż, że argumenty zawsze będą liczbami:
-//average(1) // 1
-//average(1, 3) // 2
-//average(1, 3, 6, 6) // 4
+//Napisz funkcję average, która obliczy średnią arytmetyczną wszystkich argumentów, które zostaną do niej przekazane. Załóż, że argumenty zawsze będą liczbami
 //Skorzystaj z rest parameters! Funkcję stwórz za pomocą arrow function.
 
 function average(...nums) {
