98f45a3398fd3f66f5caadf413eae1ae5d3cf269
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-02-25 16:52:41 +0100

    task 4 finished. works

--- Zadanie4.js
+++ Zadanie4.js
@@ -1,3 +1,7 @@
+//Stwórz tablicę z ocenami const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1], a następnie w umiejętny sposób przekaż oceny do funkcji average tak, aby otrzymać wynik. Skorzystaj z operatora spread!
+
+
+
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
 
 //gdy daje const average wywala
